384dbaeb3b613a08c9423b66149146c1726fc334
Jela <jere.junttila@outlook.com>
2025-06-04 22:33:10 +0300

    Fix publish (#5)
    * Remove the assumed extra file
    
    * Add nuget config to get rid of some extra source
    
    * Fix build failures
    
    * update to proper token

--- test/SoulNETLib.Tests/Extension/ClaimExtensionTests.cs
+++ test/SoulNETLib.Tests/Extension/ClaimExtensionTests.cs
@@ -1,66 +1,64 @@
-﻿using System;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Security.Principal;
 using SoulNETLib.Common.Extension;
 using SoulNETLibTests.Common.TestData.Models;
 using Xunit;
 
-namespace SoulNETLib.Tests.Extension
+namespace SoulNETLib.Tests.Extension;
+
+public class ClaimExtensionTests
 {
-    public class ClaimExtensionTests
+    #region GetUserId
+
+    [Fact]
+    public void GetUserId_SpecificallySetIdentifier_ReturnId()
     {
-        #region GetUserId
+        // Arrange
+        IPrincipal test = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new Claim[] { new Claim(ClaimTypes.NameIdentifier, SampleStrings.str1) }
+            )
+        );
 
-        [Fact]
-        public void GetUserId_SpecificallySetIdentifier_ReturnId()
-        {
-            // Arrange
-            IPrincipal test = new ClaimsPrincipal(
-                new ClaimsIdentity(
-                    new Claim[] { new Claim(ClaimTypes.NameIdentifier, SampleStrings.str1) }
-                )
-            );
+        // Act
+        var ret = test.GetUserId();
 
-            // Act
-            var ret = test.GetUserId();
-
-            // Assert
-            Assert.Equal(SampleStrings.str1, ret);
-        }
-
-        [Fact]
-        public void GetUserId_IncorrectIdentifier_ThrowsNullReferenceException()
-        {
-            // Arrange
-            IPrincipal test = new ClaimsPrincipal(
-                new ClaimsIdentity(
-                    new Claim[]
-                    { //Set only incorrect ClaimType
-                        new Claim(ClaimTypes.MobilePhone, SampleStrings.str1),
-                    }
-                )
-            );
-
-            // Act
-            Func<string> act = () => test.GetUserId();
-
-            // Assert
-            Assert.Throws<NullReferenceException>(act);
-        }
-
-        [Fact]
-        public void GetUserId_EmptyPrincipal_ThrowsNullReferenceException()
-        {
-            // Arrange
-            IPrincipal test = new ClaimsPrincipal();
-
-            // Act
-            Func<string> act = () => test.GetUserId();
-
-            // Assert
-            Assert.Throws<NullReferenceException>(act);
-        }
-
-        #endregion
+        // Assert
+        Assert.Equal(SampleStrings.str1, ret);
     }
+
+    [Fact]
+    public void GetUserId_IncorrectIdentifier_ThrowsNullReferenceException()
+    {
+        // Arrange
+        IPrincipal test = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new Claim[]
+                { //Set only incorrect ClaimType
+                    new Claim(ClaimTypes.MobilePhone, SampleStrings.str1),
+                }
+            )
+        );
+
+        // Act
+        Func<string> act = () => test.GetUserId();
+
+        // Assert
+        Assert.Throws<NullReferenceException>(act);
+    }
+
+    [Fact]
+    public void GetUserId_EmptyPrincipal_ThrowsNullReferenceException()
+    {
+        // Arrange
+        IPrincipal test = new ClaimsPrincipal();
+
+        // Act
+        Func<string> act = () => test.GetUserId();
+
+        // Assert
+        Assert.Throws<NullReferenceException>(act);
+    }
+
+    #endregion
 }
--- test/SoulNETLib.Tests/Extension/ListExtensionTests.cs
+++ test/SoulNETLib.Tests/Extension/ListExtensionTests.cs
@@ -1,62 +1,59 @@
 ﻿using SoulNETLib.Common.Extension;
 using SoulNETLibTests.Common.TestData.Models;
-using System;
-using System.Collections.Generic;
 using Xunit;
 
-namespace SoulNETLib.Tests.Extension
+namespace SoulNETLib.Tests.Extension;
+
+public class ListExtensionTests
 {
-    public class ListExtensionTests
+    [Fact]
+    public void Replace_CorrectMatch_ReplacesValue()
     {
-        [Fact]
-        public void Replace_CorrectMatch_ReplacesValue()
+        // Arrange
+        var list = new List<TestObj>
         {
-            // Arrange
-            var list = new List<TestObj>
-                {
-                    new(){ id = 1, str = SampleStrings.str1 },
-                    new(){ id = 2, str = SampleStrings.str2 },
-                    new(){ id = 3, str = SampleStrings.str3 },
-                    new(){ id = 4, str = SampleStrings.str4 },
-                    new(){ id = 5, str = SampleStrings.str5 },
-                    new(){ id = 6, str = SampleStrings.str6 },
-                };
+            new() { id = 1, str = SampleStrings.str1 },
+            new() { id = 2, str = SampleStrings.str2 },
+            new() { id = 3, str = SampleStrings.str3 },
+            new() { id = 4, str = SampleStrings.str4 },
+            new() { id = 5, str = SampleStrings.str5 },
+            new() { id = 6, str = SampleStrings.str6 },
+        };
 
-            // Act
-            var ret = list.Replace((to) => to.id == 1, new() { id = 1, str = SampleStrings.str7 });
+        // Act
+        var ret = list.Replace((to) => to.id == 1, new() { id = 1, str = SampleStrings.str7 });
 
-            // Assert
-            Assert.Equal(0, ret);
-            Assert.Equal(SampleStrings.str7, list[ret].str);
-            Assert.Equal(1, list[ret].id);
-            Assert.Equal(6, list.Count);
-        }
+        // Assert
+        Assert.Equal(0, ret);
+        Assert.Equal(SampleStrings.str7, list[ret].str);
+        Assert.Equal(1, list[ret].id);
+        Assert.Equal(6, list.Count);
+    }
 
-        [Fact]
-        public void Replace_IncorrectMatch_ReturnsNegative()
+    [Fact]
+    public void Replace_IncorrectMatch_ReturnsNegative()
+    {
+        // Arrange
+        var list = new List<TestObj>
         {
-            // Arrange
-            var list = new List<TestObj>
-                {
-                    new(){ id = 1, str = SampleStrings.str1 },
-                    new(){ id = 2, str = SampleStrings.str2 },
-                    new(){ id = 3, str = SampleStrings.str3 },
-                    new(){ id = 4, str = SampleStrings.str4 },
-                    new(){ id = 5, str = SampleStrings.str5 },
-                    new(){ id = 6, str = SampleStrings.str6 },
-                };
+            new() { id = 1, str = SampleStrings.str1 },
+            new() { id = 2, str = SampleStrings.str2 },
+            new() { id = 3, str = SampleStrings.str3 },
+            new() { id = 4, str = SampleStrings.str4 },
+            new() { id = 5, str = SampleStrings.str5 },
+            new() { id = 6, str = SampleStrings.str6 },
+        };
 
-            // Act
-            var ret = list.Replace((to) => to.id == 69, new() { id = 69, str = SampleStrings.str7 });
+        // Act
+        var ret = list.Replace((to) => to.id == 69, new() { id = 69, str = SampleStrings.str7 });
 
-            // Assert
-            Assert.Equal(-1, ret);
-        }
+        // Assert
+        Assert.Equal(-1, ret);
+    }
 
-        private sealed class TestObj
-        {
-            public int id;
-            public string? str;
-        }
+    private sealed class TestObj
+    {
+        public int id;
+        public string? str;
     }
 }
